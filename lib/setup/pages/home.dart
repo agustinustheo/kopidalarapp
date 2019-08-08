15f1412f4e29914f534ec425dfe6d6d6fe49c650
Agustinus Theodorus <agustinus.theodorus@binus.edu>
2019-08-08 23:55:54 +0700

    Added: added a new sign in feature in kopidalar app using firebase

---
+++ lib/setup/pages/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+    );
+  }
+}

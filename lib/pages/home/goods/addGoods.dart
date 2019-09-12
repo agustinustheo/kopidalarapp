c96078eeb84a0ceb292ab5a38b35c048e937f224
Agustinus Theodorus <agustinus.theodorus@binus.edu>
2019-09-12 12:38:16 +0700

    Update: trying to add image cropper and image picker (build still failed, need to migrate to AndroidX)

--- lib/pages/home/goods/addGoods.dart
+++ lib/pages/home/goods/addGoods.dart
@@ -1,7 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:kopidalar/pages/home.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 import 'package:kopidalar/pages/home/profile.dart';
 import 'package:kopidalar/util/session_util.dart';
 
@@ -10,10 +12,11 @@ class AddGoodsPage extends StatefulWidget {
   _AddGoodsState createState() => _AddGoodsState();
 }
 
-class _AddGoodsState extends State<AddGoodsPage>{
+class _AddGoodsState extends State<AddGoodsPage> with SingleTickerProviderStateMixin{
   String _userID = "";
   String _name, _price;
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  File _imageFile;
 
   _AddGoodsState() {
     getUserLogin().then((val) => setState(() {
@@ -57,28 +60,30 @@ class _AddGoodsState extends State<AddGoodsPage>{
       body: Form(
         key: _formKey,
         child: Align(
-          alignment: Alignment.center,
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              new Container(
-                width: 150.0,
-                padding: const EdgeInsets.only(
-                  bottom: 20.0, 
-                ),
-                child: new ClipRRect(
-                  borderRadius: new BorderRadius.circular(20.0),
-                  child: FadeInImage(
-                    image: NetworkImage('https://www.povertyalliance.org/wp-content/uploads/2019/03/Portrait_Placeholder.png'),
-                    placeholder: AssetImage('assets/graphics/user/anonymous.jpg'),
-                    fadeInDuration: Duration(milliseconds: 100),
-                    fadeOutDuration: Duration(milliseconds: 100),
+              new GestureDetector(
+                child: Container(
+                  width: 100.0,
+                  padding: const EdgeInsets.only(
+                    top: 20.0,
+                    bottom: 10.0, 
+                  ),
+                  child: new ClipRRect(
+                    borderRadius: new BorderRadius.circular(20.0),
+                    child: FadeInImage(
+                      image: NetworkImage('https://www.povertyalliance.org/wp-content/uploads/2019/03/Portrait_Placeholder.png'),
+                      placeholder: AssetImage('assets/graphics/user/anonymous.jpg'),
+                      fadeInDuration: Duration(milliseconds: 100),
+                      fadeOutDuration: Duration(milliseconds: 100),
+                    ),
                   ),
                 ),
+                onTap: _pickImage,
               ),
               new Container(
                 margin: const EdgeInsets.all(
-                  10.0,
+                  5.0,
                 ),
                 child: new SizedBox(
                   width: 275.0,
@@ -114,7 +119,10 @@ class _AddGoodsState extends State<AddGoodsPage>{
                       if(input.isEmpty){
                         return 'Please provide your item price';
                       }
-                      else if(input is int){
+                      try{
+                        int.parse(input);
+                      }
+                      catch(e){
                         return 'Price must be a number';
                       }
                     },
@@ -159,6 +167,20 @@ class _AddGoodsState extends State<AddGoodsPage>{
         ),
       ),
     );
+  }
+
+  Future<void> _pickImage() async{
+    File selected = await ImagePicker.pickImage(source: ImageSource.gallery);
+    selected = await ImageCropper.cropImage(
+      sourcePath: selected.path,
+      toolbarColor: Colors.brown,
+      toolbarWidgetColor: Colors.white,
+      toolbarTitle: 'Crop Image'
+    );
+
+    setState(() {
+     _imageFile = selected; 
+    });
   }
 
   Future<void> saveGoodsRecord() async{
